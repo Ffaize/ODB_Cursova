8fcb60b4c407ceb98441643ead395d806da1c5b4
Ffaize <dmitro.danko@gmail.com>
2026-03-30 12:47:19 +0300

    configured swagger

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new { Message = "Hello from CustomersController!" });
+        }
+    }
+}
